44ebfe4b3869f83fb14e8101f53723459f091533
DunkelBit <dimaldijoe@protonmail.com>
2026-06-06 17:10:45 +0530

    Added some more features, not complete

--- app/blog/[names]/page.tsx
+++ app/blog/[names]/page.tsx
@@ -1,1 +1,9 @@
 // dynamic route sample
+import { getPostbyNames } from '../../../lib/posts'
+import reactMarkDOWN from 'react-markdown'
+
+export default function postPage({ para }) {
+  const post_ = getPostbyNames(para.name)
+
+  
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { fetchAllPosts } from '../lib/posts'
+import Link from 'next/link'
 
 export default function Home() {
   const posts = fetchAllPosts()
@@ -8,7 +9,9 @@ export default function Home() {
       {
         posts.map((post) => (
          <div key={post.names} className="mb-6 border-b pb-4">
-           <h2 className="text-2xl font-semibold">{post.title}</h2>
+           <Link href={`/blog/$[name]`}>
+             <h2 className="text-2xl font-semibold">{post.title}</h2>
+           </Link>
            <p className="text-gray-500">{post.date}</p>
          </div>
        ))}
