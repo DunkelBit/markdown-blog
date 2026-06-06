16606115b10e2e1b797a59a5ef20d8a56dc0bd8a
DunkelBit <dimaldijoe@protonmail.com>
2026-06-06 12:20:17 +0530

    First stable after debugging

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import { fetchAllPosts } from '../lib/posts'
 
 export default function Home() {
-  return ('hi');
+  const posts = fetchAllPosts()
+  return (
+    <main className="max-w-2xl mx-auto p-8">
+      <h1>My Simple Blog</h1>
+      {
+        posts.map((post) => (
+         <div key={post.names} className="mb-6 border-b pb-4">
+           <h2 className="text-2xl font-semibold">{post.title}</h2>
+           <p className="text-gray-500">{post.date}</p>
+         </div>
+       ))}
+    </main>
+  )
 }
--- posts/first-post.md
+++ posts/first-post.md
@@ -1,5 +1,4 @@
 ---
-This is the first post of my markdown blog
 title: "First blog"
 date: "06-06-2026"
 tags: ["blog","welcome"]
@@ -7,4 +6,4 @@ tags: ["blog","welcome"]
 
 ## Welcome all
 
-- This is the very first blog on my markdown blog site
+This is the very first blog on my markdown blog site
